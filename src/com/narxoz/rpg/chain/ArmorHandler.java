394c57cfb43562f8aa6ab206eacba7a16abc4f51
Diana <diana.tanlabekova@narxoz.kz>
2026-03-30 09:29:20 +0500

    added logic for HPhandler and for Armorhandler

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -1,5 +1,4 @@
 package com.narxoz.rpg.chain;
-
 import com.narxoz.rpg.arena.ArenaFighter;
 
 public class ArmorHandler extends DefenseHandler {
@@ -11,8 +10,11 @@ public class ArmorHandler extends DefenseHandler {
 
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Subtract armorValue from incomingDamage; clamp the result to a minimum of 0.
-        // TODO: Print an armor message showing how much was absorbed.
-        // TODO: Pass the remaining damage to the next handler.
+        int absorbed = Math.min(incomingDamage, armorValue);
+        int remainingDamage = Math.max(0, incomingDamage - armorValue);
+        
+        System.out.println("[Armor] " + target.getName() + "'s armor absorbed " + absorbed + " damage!");
+        passToNext(remainingDamage, target);
     }
 }
+
